cc73997e38f6139463adcb2705d2d37e39c52ceb
Saksham Srivastava <saksham.srivastava@oracle.com>
2026-03-22 17:13:06 +0530

    add code commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import "./index.css"; // optional, if you have global styles
+import "./index.css"; // optional if you have global styles
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
